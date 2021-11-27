32769db1692b17c2ebd704262eaf772ba4c22f62
CL0Pinette <cl0pinette@protonmail.com>
2021-11-27 04:08:38 +0100

    ajout sidebar

--- front/src/Components/Sidebar.js
+++ front/src/Components/Sidebar.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import styles from './Sidebar.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "../assets/Sidebar.css";
+import img from "../assets/burger-menu.svg";
+
+var element;
+var trigger;
+
+function onload() {
+  element = document.getElementById("wrapper");
+  trigger = document.getElementById("menu-toggle"); 
+  trigger.addEventListener("click", function (e) {
+    e.preventDefault();
+    element.classList.toggle("toggled");
+  });
+}
 
 const Sidebar = () => {
   return (
-    <div className={styles.SidebarContainer}>
-		<p>TEMPLATE</p>
+    <div id="wrapper">
+      <div id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+          <li class="sidebar-brand">
+            <img class="rounded" width="75px" src="https://img.le-dictionnaire.com/a-blason.jpg"></img>
+            <a href="#">Name </a>
+          </li>
+          <li>
+            <a href="#">Contacts </a>
+          </li>
+          <li>
+            <a href="#">Commandes </a>
+          </li>
+        </ul>
+      </div>
+      <div class="page-content-wrapper">
+        <div class="container-fluid">
+        <a class="btn btn-link" role="button" id="menu-toggle" href="#menu-toggle" onLoad={onload}>
+          <img src={img}></img>
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
