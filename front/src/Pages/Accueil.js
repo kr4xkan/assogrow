410f202d4bdc2095c2e83a2793d1b11d9bcd9494
kr4xkan <kr4xkan@gmail.com>
2021-11-27 10:51:35 +0100

    acceuil

--- front/src/Pages/Accueil.js
+++ front/src/Pages/Accueil.js
@@ -6,11 +6,14 @@ const AccueilPage = () => {
     return (
         <div class="AccueilContainer">
             <div class="header">
-                <p class="bang">LA PLATEFORME DE COMMERCE LOCAL ET SAISONNIER</p>
-                <p class="selectorText">JE SUIS UN.E</p>
-                <div class="buttons">
-                    <button>PRODUCTEUR</button>
-                    <button>ENTREPRISE</button>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 495">
+                <path id="svg_1" d="m-5.33333,48.33331l83.55556,2.42592c98.22223,9.09259 270.66668,36.61111 432.44446,107.13531c161.77778,69.86355 323.55556,123.05105 485.33335,113.63681c161.77778,-8.75359 295.55556,-35.79062 376.44445,-88.64248l80.88889,-52.85186l0,369.96299l-80.88889,0c-131.44445,0 -242.66667,0 -404.44446,0c-161.77778,0 -323.55556,0 -485.33335,0c-161.77778,0 -323.55556,0 -404.44446,0l-80.88889,0l-2.66667,-451.66669z" fill="#97dc8a"/>
+                </svg>
+                <p class="bang">ACHETEZ DES LEGUMES A VOTRE VOISIN</p>
+                <div class="inputcontainer">
+                    <p>Recherchez un produit:</p>
+                    <input type="text" placeholder="Tomates, Oeufs, ..." />
+                    <button>&#62;</button>
                 </div>
                 <Link class="login" to="/login">SE CONNECTER</Link>
             </div>
