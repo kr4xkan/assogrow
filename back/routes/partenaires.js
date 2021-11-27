6d5325cf1921ae98b0406c1a3b779c0f816a7272
CL0Pinette <cl0pinette@protonmail.com>
2021-11-27 20:19:53 +0100

    ajout route /partenaires/all

--- back/routes/partenaires.js
+++ back/routes/partenaires.js
@@ -5,6 +5,18 @@ const { body, validationResult } = require('express-validator');
 const config = require('../config');
 const Partenaire = require('../models/Partenaire');
 
+router.get('/all', async(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ error: errors.array() });
+    }
+    fetchUserByToken(req).then(async(doc) => {
+            let allPartenaires = Partenaire.find({ asso: _id }).exec();
+            return res.status(200).json(allPartenaires);
+        })
+        .catch(err => res.status(401).json({ err }));
+})
+
 router.post('/add', [
     body('name').exists().isString(),
     body('contact').exists().isString(),
@@ -15,17 +27,17 @@ router.post('/add', [
     }
     const _name = req.body.name;
     const _contact = req.body.contact;
-    fetchUserByToken(req).then(async (doc) => {
-        let data = {
-            asso: doc._id,
-            name: _name,
-            contact: _contact
-        };
-        let newPartenaire = new Partenaire(data);
-        await newPartenaire.save();
-        return res.status(200);
-    })
-    .catch(err => res.status(401).json({ err }));
+    fetchUserByToken(req).then(async(doc) => {
+            let data = {
+                asso: doc._id,
+                name: _name,
+                contact: _contact
+            };
+            let newPartenaire = new Partenaire(data);
+            await newPartenaire.save();
+            return res.status(200);
+        })
+        .catch(err => res.status(401).json({ err }));
 });
 
 router.put('/:id', [
@@ -40,43 +52,43 @@ router.put('/:id', [
     const _contact = req.body.contact;
 
     fetchUserByToken(req).then((doc) => {
-        Partenaire.findOne({ _id: req.params.id }, async function (err, c) {
-            if (err) {
-                return res.status(400).json({ error: errors.array() });
-            }
-            if (asso !== doc._id) {
-                return res.status(401).json({ error: errors.array() });
-            }
-            c.contact = _contact;
-            c.name = _name;
-            await c.save();
-            res.status(200).send();
-        });
-    })
-    .catch(err => res.status(401).json({ error: err }));
+            Partenaire.findOne({ _id: req.params.id }, async function(err, c) {
+                if (err) {
+                    return res.status(400).json({ error: errors.array() });
+                }
+                if (asso !== doc._id) {
+                    return res.status(401).json({ error: errors.array() });
+                }
+                c.contact = _contact;
+                c.name = _name;
+                await c.save();
+                res.status(200).send();
+            });
+        })
+        .catch(err => res.status(401).json({ error: err }));
 });
 
 router.delete('/:id', [
     body('name').exists().isString(),
 ], (req, res) => {
     fetchUserByToken(req).then((doc) => {
-        Partenaire.findOne({ _id: req.params.id }, async function(err, c) {
-            if (err) {
-                return res.status(400).json({ err });
-            }
-            if (asso !== doc._id) {
-                return res.status(401).json();
-            }
-            await Partenaire.deleteOne({_id:req.params.id}).exec();
-            res.status(200).send();
-        });
-    })
-    .catch(err => res.status(401).json({ err }));
+            Partenaire.findOne({ _id: req.params.id }, async function(err, c) {
+                if (err) {
+                    return res.status(400).json({ err });
+                }
+                if (asso !== doc._id) {
+                    return res.status(401).json();
+                }
+                await Partenaire.deleteOne({ _id: req.params.id }).exec();
+                res.status(200).send();
+            });
+        })
+        .catch(err => res.status(401).json({ err }));
 });
 
 function fetchUserByToken(req) {
-    return new Promise((resolve,reject) => {
-        if(req.headers && req.headers.authorization) {
+    return new Promise((resolve, reject) => {
+        if (req.headers && req.headers.authorization) {
             let authorization = req.headers.authorization
             if (authorization.startsWith('Bearer ')) {
                 authorization = authorization.slice(7, authorization.length)
@@ -85,12 +97,12 @@ function fetchUserByToken(req) {
             try {
                 decoded = JWT.verify(authorization, config.jwtsecret)
             } catch (e) {
-                reject('Token unvalid, '+e)
+                reject('Token unvalid, ' + e)
                 return
             }
-            User.findOne({_id: decoded.id}, (err, doc) => {
+            User.findOne({ _id: decoded.id }, (err, doc) =>  {
                 if (err) {
-                    reject('Token error,'+err)
+                    reject('Token error,' + err)
                 }
                 if (doc.accessToken == authorization)
                     resolve(doc)
