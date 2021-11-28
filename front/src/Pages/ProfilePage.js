7c2d238d49f5ceafc6c94d2daf0f1a775177225d
CL0Pinette <cl0pinette@protonmail.com>
2021-11-28 03:54:17 +0100

    Revert "add profile page"
    This reverts commit b91ab03fe362bf15fc3223cf34cce39fec93c816.

--- front/src/Pages/ProfilePage.js
+++ front/src/Pages/ProfilePage.js
@@ -1,15 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import "../assets/Sidebar.css";
-import AuthContext from "../AuthContext";
 
 const ProfilePage = () => {
-  let context = useContext(AuthContext);
   return (
     <div id="profile">
-      <h3 class="title">Page de profil de {context.name}</h3>
-      <p class="text">
-        Votre email : {context.email}
-      </p>
+		<h1>PROFILE PAGE</h1>
     </div>
   );
 };
