b91ab03fe362bf15fc3223cf34cce39fec93c816
CL0Pinette <cl0pinette@protonmail.com>
2021-11-28 03:52:48 +0100

    add profile page

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,16 @@
 import React, { useContext } from "react";
 import { Routes, Route, Navigate, Outlet } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import AuthContext from "./AuthContext";
 
-import AccueilPage from './Pages/Accueil';
-import DashboardPage from './Pages/Dashboard';
+import AccueilPage from "./Pages/Accueil";
+import DashboardPage from "./Pages/Dashboard";
 import DossierPage from "./Pages/Dossier";
-import LoginPage from './Pages/Login';
+import LoginPage from "./Pages/ProfilePage";
 import PartnerPage from "./Pages/Partner";
 import StocksPage from "./Pages/Stocks";
 
 function App() {
-
   return (
     <Routes>
       <Route path="/" element={<AccueilPage />} />
@@ -19,19 +18,17 @@ function App() {
       <Route path="/p" element={<PartnerPage />} />
       <Route path="/s" element={<StocksPage />} />
       <Route path="login" element={<LoginPage />} />
-
-      <Route path='/dashboard' element={<PrivateRoute/>}>
-        <Route path='/dashboard' element={<DashboardPage/>}/>
+      <Route path="/dashboard" element={<PrivateRoute />}>
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Route>
     </Routes>
   );
 }
 
-
-const PrivateRoute = ({component: Component, auth, ...rest}) => {
+const PrivateRoute = ({ component: Component, auth, ...rest }) => {
   const { isLoggedIn } = useContext(AuthContext);
 
   return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
-}
+};
 
 export default App;
